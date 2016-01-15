de06b43f0b5ad04d3026ea9e452fd6157205e6b3
fgabriel <fgabrieli@gmail.com>
2016-01-14 21:38:28 -0300

    Added very simple doc.

--- js/modules/module.js
+++ js/modules/module.js
@@ -2,7 +2,9 @@ neo.Module = {
   tag : {},
   
   /**
-   * @param Object with options or false.
+   * @param Object with options or false. 
+   * 
+   * Options are loaded from the "data-options" attribute set in the tag element. It accepts any valid javascript expression.
    */
   construct : function(options) {
     // overridden by modules
